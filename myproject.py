01a69196e25036b9fe2f0595bfb1d338ec2392b5
AndrewDongminYoo <ydm2790@gmail.com>
2021-09-30 01:45:34 +0900

    for HTTPS ::🔥

--- myproject.py
+++ myproject.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, url_for, render_template, Response  # 플라스크 기본 메소드
+from flask import Flask, request, jsonify, render_template, Response
 from pymongo import MongoClient  # 몽고디비
 import requests  # 서버 요청 패키지
 import json  # json 응답 핸들링
@@ -128,5 +128,5 @@ def search_add():
 
 
 if __name__ == '__main__':
-    # app.run('0.0.0.0', port=5000)
-    app.run('0.0.0.0', port=80)
+    app.run('0.0.0.0', port=5000)
+    # app.run('0.0.0.0', port=80)
